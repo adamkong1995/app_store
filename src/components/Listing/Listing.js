242eb512ebb4943c97d857c32ffe5e1eb68c2769
adamkong1995 <adamkong256@gmail.com>
2019-07-17 10:08:10 +0800

    Added loading spinner

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -7,21 +7,30 @@ import { fetchAppList, setPageNumber } from '../../actions';
 import ListItem from './ListItem';
 import Pagination from './Pagination';
 
+import { FaSpinner } from 'react-icons/fa';
+
+import './Listing.css';
+
 class Listing extends Component {
     componentDidMount() {
         this.props.fetchAppList(this.props.pageNum);
-    }
+    };
 
     componentDidUpdate(prev) {
         if(prev.pageNum !== this.props.pageNum) {
             this.props.fetchAppList(this.props.pageNum);
-        }
+        };
         if(prev.keyword !== this.props.keyword) {
             this.props.fetchAppList(this.props.pageNum, this.props.keyword);
-        }
-    }
+            this.props.setPageNumber('set', 1);
+        };
+    };
 
     renderItem () {
+        //Display loading indicator when fetching data
+        if (this.props.appList.length === 0 ){
+            return <FaSpinner />
+        };
         return _.map(this.props.appList, ({artworkUrl512, trackName, primaryGenreName, averageUserRating, userRatingCount, ranking})=> {
             return <ListItem icon={artworkUrl512} name={trackName} genre={primaryGenreName} rating={averageUserRating} userCount={userRatingCount} ranking={ranking} />
         });
